6f0edfcff76e4b5dd641e0f7c80794498fb51157
Martin Penev <martin.penev@accedia.com>
2019-07-04 16:25:03 +0300

    feat(delete): implement lambda for deleting users and their images in s3

---
+++ delete/deleteUsers/index.js
@@ -0,0 +1,42 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+const ddb = new AWS.DynamoDB;
+const srcBucket = "accedia-users-avatars";
+AWS.config.update( {region:"us-east-2"} );
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = "Users";
+
+exports.handler = async (event) => {
+    let username = event.username;
+    console.log("Deleting " + username + "...");
+    const imageToDelete = {
+        Bucket: srcBucket,
+        Key: username
+    } 
+    const userToDelete = {
+        TableName : table,
+        Key : {
+            "username" : username
+        } 
+    };
+    const userToDeleteCheck = {
+        TableName: table,
+        ProjectionExpression: "username",
+        KeyConditionExpression: "username = :user",
+        ExpressionAttributeValues: {
+            ":user": {
+                S: event.username
+            }
+        }
+    }
+    const userFound = await ddb.query(userToDeleteCheck).promise();
+    let result;
+    if(userFound.Count == 0){
+        result = "No such user";
+    }else{
+        await s3.deleteObject(imageToDelete).promise();
+        await docClient.delete(userToDelete).promise();
+        result = userToDelete.Key;
+    }
+    return result;
+}
