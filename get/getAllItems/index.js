1cd3feda3146aac6c22c61db7c7dd88e85e692e6
Martin Penev <martin.penev@accedia.com>
2019-07-04 11:36:27 +0300

    New changes on the styling of the code

--- get/getAllItems/index.js
+++ get/getAllItems/index.js
@@ -1,27 +1,20 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update(
-    {
-        region: 'us-east-2'
-    }
-);
+AWS.config.update( {region: 'us-east-2'} );
 
 exports.handler = async (event) => {    
     const ddb = new AWS.DynamoDB();    
-    const params = 
-    {
+    const params = {
         "TableName" : "Users"
     };
 
     console.log("Fetching all users from db...");
     let result;
     
-    try
-    {
+    try {
         result = (await ddb.scan(params).promise()).Items;
     }
-    catch(err)
-    {
+    catch(err) {
         result = err.message;
     }
 
