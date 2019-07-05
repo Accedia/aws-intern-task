61f3fd6c51d8b4aa7b8ac35da65a1795430cd160
Martin Penev <martin.penev@accedia.com>
2019-07-05 15:32:29 +0300

    fix: implement lambda for getting all the users from a db

--- get/getAllItems/index.js
+++ get/getAllItems/index.js
@@ -1,21 +1,39 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update( {region: 'us-east-2'} );
+AWS.config.update(
+    {
+        region: 'us-east-2'
+    }
+);
 
 exports.handler = async (event) => {    
     const ddb = new AWS.DynamoDB();    
-    const params = {
+    const params = 
+    {
         "TableName" : "Users"
     };
 
     console.log("Fetching all users from db...");
     let result;
     
-    try {
-        result = (await ddb.scan(params).promise()).Items;
+    try
+    {
+        result ={
+            statusCode: 200,
+            body: (await ddb.scan(params).promise()).Items
+        } 
+        
+        for(let value in result.body){
+            result.body[value] = result.body[value];
+        }
+        result.body = JSON.stringify(result.body);
     }
-    catch(err) {
-        result = err.message;
+    catch(err)
+    {
+        result = {
+            statusCode: 500,
+            body: err.message
+        }
     }
 
     return result;    
