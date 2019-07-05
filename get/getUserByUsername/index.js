3232d6484f51a35c7d786bbfce5acbf7b5874d54
Martin Penev <martin.penev@accedia.com>
2019-07-05 15:26:56 +0300

    fix: lambda works from API correctlyy

--- get/getUserByUsername/index.js
+++ get/getUserByUsername/index.js
@@ -1,16 +1,16 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const ddb = new AWS.DynamoDB();
+// const ddb = new AWS.DynamoDB();
 const table = "Users";
 AWS.config.update({region : "us-east-2"});
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const username = event.username;
+    const username = event.pathParameters.username;
     const userWithUsername = {
         TableName: table,
-        KeyConditionExpression: "#user = :user",
-        ExpressionAttributeNames:{
-            "#user": "username"
+        Key : {
+            "username": username
         },
         ExpressionAttributeValues: {
             ":user":{
@@ -18,12 +18,19 @@ exports.handler = async (event) => {
             }
         }   
     }
-    const userResults = await ddb.query(userWithUsername).promise();
-    let result;
-    if(userResults.Count == 0){
-        result = "No such user";
+    const userResults = await documentClient.get(userWithUsername).promise();
+    console.log(userResults);
+    let result = {};
+    if(!userResults.Item){
+        result["statusCode"] = 404;
+        result["body"] = "No such user";
     }else{
-        result = userResults.Items[0];
+        result["statusCode"] = 200;
+        result["body"] = userResults.Item;
+        for(let value in result.body){
+            result.body[value] = result.body[value];
+        }
+        result["body"] = JSON.stringify(result["body"]);
     }
     return result;
 }
