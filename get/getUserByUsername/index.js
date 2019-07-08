082374fcd8e7e2ebe4a302442704794a006596fb
Martin Penev <martin.penev@accedia.com>
2019-07-08 12:55:05 +0300

    feat(get all users with username): lambda function now returns all the items that have username containing the string provided by the user

--- get/getUserByUsername/index.js
+++ get/getUserByUsername/index.js
@@ -1,35 +1,31 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-// const ddb = new AWS.DynamoDB();
+const ddb = new AWS.DynamoDB();
 const table = "Users";
 AWS.config.update({region : "us-east-2"});
-const documentClient = new AWS.DynamoDB.DocumentClient();
+// const documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     const username = event.pathParameters.username;
     const userWithUsername = {
         TableName: table,
-        Key : {
-            "username": username
-        },
+        FilterExpression: `contains(username, :user)`,
         ExpressionAttributeValues: {
-            ":user":{
+            ":user": {
                 S: username
             }
-        }   
+        }
     }
-    const userResults = await documentClient.get(userWithUsername).promise();
+    
+    const userResults = await ddb.scan(userWithUsername).promise();
     console.log(userResults);
     let result = {};
-    if(!userResults.Item){
+    if(!userResults.Count){
         result["statusCode"] = 404;
         result["body"] = "No such user";
     }else{
         result["statusCode"] = 200;
-        result["body"] = userResults.Item;
-        for(let value in result.body){
-            result.body[value] = result.body[value];
-        }
+        result["body"] = userResults.Items;
         result["body"] = JSON.stringify(result["body"]);
     }
     return result;
