82cdc8dc1dea60693bacb3eab65a0796bfaa9899
Martin Penev <martin.penev@accedia.com>
2019-07-10 11:23:21 +0300

    feat(frontned-avatar display): frontend now displaying avatar images of users, also made responsive

--- frontend/users-avatars/src/app/app.component.ts
+++ frontend/users-avatars/src/app/app.component.ts
@@ -9,51 +9,70 @@ import { UsersService } from 'src/services/users.service';
 
 export class AppComponent implements OnInit {
   public title = 'users-avatars';
-  public users: any[];
+  public displayedUsers: any[] = [];
+  public allUsers: any[] = [];
   public success = null;
   public error = null
+  private messageDisplayTimeMs = 3000;
 
   public constructor(private userService: UsersService) {}
   
   public ngOnInit() {
     this.userService.list().subscribe(users => {
-      this.users = users;
+      this.displayedUsers = users;
+      this.allUsers = users;
     });
   }  
   
   public search(event: any){
-    this.userService.searchByName(event.target.value).subscribe(users => {
-        this.users = users
-    }, error => {
-        this.users = null;
-    })    
+    let searchedUsers:any[] = [];
+    let searchingFor = event.target.value.toLowerCase();
+    for(var i = 0; i < this.allUsers.length; i++){
+      if(this.allUsers[i].username.S.toLowerCase().includes(searchingFor)){
+        searchedUsers.push(this.allUsers[i]);
+      }
+    }
+    this.displayedUsers = searchedUsers;
   }
 
   public delete(username) {
-    for( var i = 0; i < this.users.length; i++){ 
-      if (this.users[i].username.S === username) {
-        this.users.splice(i, 1); 
+    for( var i = 0; i < this.displayedUsers.length; i++){ 
+      if (this.displayedUsers[i].username.S === username) {
+        this.displayedUsers.splice(i, 1); 
       }
     }
     this.userService.deleteByName(username).subscribe(deletedUser => {
-      console.log(deletedUser);
-      this.updateSuccess(deletedUser.username + " successfully deleted!", 3000);
+      this.updateSuccess(deletedUser.username + " successfully deleted!");
     }, error => {
-      this.updateError(error, 3000);
+      this.updateError(error);
     });
   }
 
-  updateSuccess(message: string, delayDestroy: number){
-    this.success = message;
-    setTimeout(() => {
-      this.success=null;
-    }, delayDestroy);
+  private capitalize(str: string):string{
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  updateError(message: string, delayDestroy: number){
-    this.error = message;
+  openModal(avatarUrl: string):void{
+    document.getElementById("avatar-modal").style.display = "block";
+    let avatarImage = document.getElementById("avatar-image") as HTMLImageElement;
+    avatarImage.src = avatarUrl;
+  }
+
+  closeModal(){
+    document.getElementById("avatar-modal").style.display = "none";
+  }
+
+  updateSuccess(message: string):void{
+    this.success = this.capitalize(message);
+    setTimeout(() => {
+      this.success=null;
+    }, this.messageDisplayTimeMs);
+  }
+
+  updateError(message: string):void{
+    this.error = this.capitalize(message);
     setTimeout(() => {
       this.error=null;
-    }, delayDestroy);
+    }, this.messageDisplayTimeMs);
   }
 }
