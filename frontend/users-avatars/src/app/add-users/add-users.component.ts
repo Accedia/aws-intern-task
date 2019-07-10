bc1b6b45bd5b6b66f632a89833e4e3593006f2f9
Martin Penev <martin.penev@accedia.com>
2019-07-10 14:04:51 +0300

    fix(frontend - send avatar image): make frontend send correct base64 string of avatar images

--- frontend/users-avatars/src/app/add-users/add-users.component.ts
+++ frontend/users-avatars/src/app/add-users/add-users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/services/users.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Input, Output, EventEmitter } from '@angular/core';
+import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-add-users',
@@ -15,14 +16,14 @@ export class AddUsersComponent implements OnInit{
   @Output() error = new EventEmitter<string>();
   showRegister = false; 
   userFormReady = false;
-
+  imageBase64;
   public userForm = new FormGroup({
     username: new FormControl(''),
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     password: new FormControl(''),
     email: new FormControl(''),
-    image: new FormControl()
+    image: new FormControl('')
   });
   
   constructor(private usersService: UsersService) {}
@@ -30,8 +31,11 @@ export class AddUsersComponent implements OnInit{
   ngOnInit(){}
 
   private onSubmit(){
+
+    this.userForm.value.image = this.imageBase64;
     this.usersService.addUser(this.userForm.value).subscribe(newUser => {
         this.success.emit("Successfully added user " + newUser.username.S);
+        console.log(this.userForm.value);
         this.users.push(newUser);
     }, error => {
         this.error.emit(error.error);
@@ -42,14 +46,15 @@ export class AddUsersComponent implements OnInit{
     this.showRegister = !this.showRegister;  
   }
 
-  private getBase64(event: any) {
+  public getBase64(event: any) {
     let me = this;
     let file = event.target.files[0];
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
-      me.userForm.value.image = reader.result;
+      me.imageBase64 = reader.result;
       me.userFormReady = true;
+      console.log(me.userForm.value);
     };
     reader.onerror = function (error) {
       console.log(error);
